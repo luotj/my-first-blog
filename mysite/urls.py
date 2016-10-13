2d158a4b4cc008066ec984372c24c86521807fd8
luotj <simpleltj@gmail.com>
2016-10-13 22:04:36 +0800

    add login and comment

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import django.contrib.auth.views
+from django.contrib.auth import views
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
 
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^admin/', include(admin.site.urls)),
 ]
